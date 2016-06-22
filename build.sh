da8d03d348189e24f3998e3fba8e7c8411997db7
Simba Zhang <zzhang@actiontec.com>
2016-06-22 13:19:16 -0700

    Patch the KurentoToolbox.h for the mismatch <>.

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ pod install
 cp -rf patchs/RTCTypes.h Pods/libjingle_peerconnection/libjingle_peerconnection/Headers/RTCTypes.h
 cp -rf patchs/RTCPeerConnection.h Pods/libjingle_peerconnection/libjingle_peerconnection/Headers/RTCPeerConnection.h
 cp -rf patchs/RTCSessionDescription.h Pods/libjingle_peerconnection/libjingle_peerconnection/Headers/RTCSessionDescription.h
+cp -rf patchs/KurentoToolbox.h Pods/Headers/Private/KurentoToolbox/KurentoToolbox.h
 
 xcodebuild -workspace Kurento-iOS.xcworkspace -scheme KurentoToolbox -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
