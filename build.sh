8680a78a72a0f2acba7346fb23392ff22342a6d4
Simba Zhang <zzhang@actiontec.com>
2016-06-22 13:15:08 -0700

    Force over write the patch headers.

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 pod install
 
-cp patchs/RTCTypes.h Pods/libjingle_peerconnection/libjingle_peerconnection/Headers/RTCTypes.h
-cp patchs/RTCPeerConnection.h Pods/libjingle_peerconnection/libjingle_peerconnection/Headers/RTCPeerConnection.h
-cp patchs/RTCSessionDescription.h Pods/libjingle_peerconnection/libjingle_peerconnection/Headers/RTCSessionDescription.h
+cp -rf patchs/RTCTypes.h Pods/libjingle_peerconnection/libjingle_peerconnection/Headers/RTCTypes.h
+cp -rf patchs/RTCPeerConnection.h Pods/libjingle_peerconnection/libjingle_peerconnection/Headers/RTCPeerConnection.h
+cp -rf patchs/RTCSessionDescription.h Pods/libjingle_peerconnection/libjingle_peerconnection/Headers/RTCSessionDescription.h
 
 xcodebuild -workspace Kurento-iOS.xcworkspace -scheme KurentoToolbox -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
